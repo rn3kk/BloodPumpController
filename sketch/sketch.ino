6ec5cafbb29e7e726dae9c4e76f61287b094bcb8
nbanshchikov <nbanschikov@gmail.com>
2022-04-20 16:23:58 +0300

    first

---
+++ sketch/sketch.ino
@@ -0,0 +1,17 @@
+const int pwm_pin = 3;
+
+const int speed_sin = 5;
+const int max_v = 255;  //from 0..255
+const float freq = 0.0015;
+const int min_v = 75;  // from 0..255
+
+void setup() {
+  pinMode(pwm_pin, OUTPUT);
+
+}
+
+void loop() {  
+  int value = abs(sin(millis() * freq)) * (max_v-min_v) + min_v;
+  analogWrite(pwm_pin, value);  
+  
+}
