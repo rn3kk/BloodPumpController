34c8590265d390af07be51eec12604da91d33b56
Nikolay Banshchikov <rn3kk.nick@gmail.com>
2022-06-02 10:15:44 +0300

    Update sketch.ino
    Заменили частоту на удары в минуту

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,17 +1,19 @@
 const int pwm_pin = 3;
 
 const int max_v = 255;  //from 0..255  
-const int min_v = 75;  // from 0..255 
-const float freq = 0.0015;
+const int min_v = 0;  // from 0..255 
+const float hear_rate = 60; //in minutes
 
+
+float hear_ms;
 
 void setup() {
   pinMode(pwm_pin, OUTPUT);
+  hear_ms = hear_rate/60/1000;
 
 }
 
 void loop() {  
-  int value = abs(sin(millis() * freq)) * (max_v-min_v) + min_v;
+  int value = abs(sin( millis() * hear_ms *2 )) * (max_v-min_v) + min_v;
   analogWrite(pwm_pin, value);  
-  
 }
